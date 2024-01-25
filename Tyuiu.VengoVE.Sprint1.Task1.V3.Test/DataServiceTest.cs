74d37f54f8e12795ea1b56ef9efbb43805f2cf7c
VengoVE <v726562@gmail.com>
2024-01-25 15:52:08 +0300

    Task1

---
+++ Tyuiu.VengoVE.Sprint1.Task1.V3.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.VengoVE.Sprint1.Task1.V3.Lib;
+
+namespace Tyuiu.VengoVE.Sprint1.Task1.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpession()
+        {
+
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(-1, res);
+        }
+    }
+}
---
+++ Tyuiu.VengoVE.Sprint1.Task1.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VengoVE.Sprint1.Task1.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VengoVE.Sprint1.Task1.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("56c0e25d-ec0b-47a4-86f6-45fff6b55175")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
